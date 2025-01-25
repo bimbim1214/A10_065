3504c0420ca2e23a817deba342a554d2bdde330f
bimbim1214 <bimoadityapangestu@gmail.com>
2025-01-25 12:29:06 +0700

    Menambahkan ekstensi fungsi Tanaman.toInsertUiEvent untuk memetakan data Tanaman ke InsertUiEvent

---
+++ app/src/main/java/com/example/uas_pam/ui/viewmodel/tanaman/InsertTnmViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.uas_pam.ui.viewmodel.tanaman
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.uas_pam.model.Tanaman
+import com.example.uas_pam.repository.TanamanRepository
+import kotlinx.coroutines.launch
+
+
+fun Tanaman.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
+    id_tanaman = id_tanaman,
+    nama_tanaman = nama_tanaman,
+    periode_tanam = periode_tanam,
+    deskripsi_tanaman = deskripsi_tanaman
+)
