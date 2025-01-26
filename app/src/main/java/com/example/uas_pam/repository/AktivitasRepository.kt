dd6dab4e5f196c0374f937f273c93c47018f0b34
bimbim1214 <bimoadityapangestu@gmail.com>
2025-01-26 10:32:34 +0700

    Menambahkan implementasi NetworkAktivitasRepository untuk operasi CRUD aktivitas pertanian

--- app/src/main/java/com/example/uas_pam/repository/AktivitasRepository.kt
+++ app/src/main/java/com/example/uas_pam/repository/AktivitasRepository.kt
@@ -13,3 +13,40 @@ interface AktivitasPertanianRepository{
     suspend fun getAktivitasById(idAktivitas: String): AktivitasPertanian
 }
 
+class NetworkAktivitasRepository(
+    private val aktivitasService: AktivitasService
+): AktivitasPertanianRepository{
+    override suspend fun insertAktivitas(aktivitasPertanian: AktivitasPertanian) {
+        aktivitasService.insertAktivitas(aktivitasPertanian)
+    }
+
+    override suspend fun updateAktivitas(
+        idAktivitas: String,
+        aktivitasPertanian: AktivitasPertanian
+    ) {
+        aktivitasService.updateAktivitas(idAktivitas, aktivitasPertanian)
+    }
+
+    override suspend fun deleteAktivitas(idAktivitas: String) {
+        try {
+            val response = aktivitasService.deleteAktivitas(idAktivitas)
+            if (!response.isSuccessful){
+                throw IOException("Failled to delete aktivitas. HTTP Status code: "+
+                "${response.code()}")
+            }else{
+                response.message()
+                println(response.message())
+            }
+        }catch (e: Exception){
+            throw e
+        }
+    }
+
+    override suspend fun getAktivitas(): AktivitasPertanianResponse {
+        return aktivitasService.getAktivitas()
+    }
+
+    override suspend fun getAktivitasById(idAktivitas: String): AktivitasPertanian {
+        return aktivitasService.getAktivitasById(idAktivitas).data
+    }
+}
