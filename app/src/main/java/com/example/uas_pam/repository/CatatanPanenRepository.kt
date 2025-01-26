c20956224f1dd76fa83e1f7b2064885c23a5fc6e
bimbim1214 <bimoadityapangestu@gmail.com>
2025-01-26 10:30:50 +0700

    Implementasi NetworkCatatanPanenRepository untuk melakukan operasi CRUD catatan panen melalui API

--- app/src/main/java/com/example/uas_pam/repository/CatatanPanenRepository.kt
+++ app/src/main/java/com/example/uas_pam/repository/CatatanPanenRepository.kt
@@ -13,3 +13,37 @@ interface CatatanPanenRepository {
     suspend fun getPanenById(idPanen: String): CatatanPanen
 }
 
+class NetworkCatatanPanenRepository(
+    private val panenApiService: PanenService
+): CatatanPanenRepository {
+    override suspend fun insertPanen(catatanPanen: CatatanPanen) {
+        panenApiService.insertPanen(catatanPanen)
+    }
+
+    override suspend fun updatePanen(idPanen: String, catatanPanen: CatatanPanen) {
+        panenApiService.updatePanen(idPanen, catatanPanen)
+    }
+
+    override suspend fun deletePanen(idPanen: String) {
+        try {
+            val response = panenApiService.deletePanen(idPanen)
+            if (!response.isSuccessful){
+                throw IOException("Failled to delete Panen. HTTP Status code: "+
+                "${response.code()}")
+            }else{
+                response.message()
+                println(response.message())
+            }
+        }catch (e: Exception){
+            throw e
+        }
+    }
+
+    override suspend fun getPanen(): CatatanPanenResponse {
+        return panenApiService.getPanen()
+    }
+
+    override suspend fun getPanenById(idPanen: String): CatatanPanen {
+        return panenApiService.getPanenById(idPanen).data
+    }
+}
