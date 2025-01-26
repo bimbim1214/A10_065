78d6307af25a0f490f372c290043e10e0f01c79c
bimbim1214 <bimoadityapangestu@gmail.com>
2025-01-26 10:23:13 +0700

    Tambah UpdateScreen untuk halaman pembaruan data tanaman dengan top bar dan fungsi simpan

---
+++ app/src/main/java/com/example/uas_pam/ui/view/tanaman/UpdateTnmView.kt
@@ -0,0 +1,59 @@
+package com.example.uas_pam.ui.view.tanaman
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.uas_pam.ui.customwidget.CostumeTopAppBar
+import com.example.uas_pam.ui.navigation.DestinasiNavigasi
+import com.example.uas_pam.ui.viewmodel.PenyediaViewModel
+import com.example.uas_pam.ui.viewmodel.tanaman.UpdateTnmViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UpdateScreen(
+    onBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    onNavigate:()-> Unit,
+    viewModel: UpdateTnmViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiUpdate.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUP = onBack,
+            )
+        }
+    ){padding ->
+        EntryBody(
+            modifier = Modifier.padding(padding),
+            insertUiState = viewModel.updaUiState,
+            onTanamanValueChange = viewModel::updateInsertTnmState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updateTnm()
+                    delay(600)
+                    withContext(Dispatchers.Main){
+                        onNavigate()
+                    }
+                }
+            }
+        )
+    }
+}
