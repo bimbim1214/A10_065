f9f40e9f85eea5bd5d11408b1d2295a9da1f831b
bimbim1214 <bimoadityapangestu@gmail.com>
2025-01-27 20:47:38 +0700

    memanggil kebuApp untuk menjalankan aplikasi

---
+++ app/src/main/java/com/example/uas_pam/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.uas_pam
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.uas_pam.ui.theme.UAS_PAMTheme
+import com.example.uas_pam.ui.viewmodel.KebunApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            UAS_PAMTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    KebunApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    UAS_PAMTheme {
+        Greeting("Android")
+    }
+}
