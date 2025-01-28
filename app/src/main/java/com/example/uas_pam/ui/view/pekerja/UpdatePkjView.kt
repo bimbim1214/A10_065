d80bd56cf18d93ab00b30815ac1f21ccc8f3932b
bimbim1214 <bimoadityapangestu@gmail.com>
2025-01-28 19:51:20 +0700

    UpdateScreenPekerja untuk memperbarui data pekerja dengan dukungan untuk navigasi dan aksi simpan

---
+++ app/src/main/java/com/example/uas_pam/ui/view/pekerja/UpdatePkjView.kt
@@ -0,0 +1,60 @@
+package com.example.uas_pam.ui.view.pekerja
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.uas_pam.ui.customwidget.CostumeTopAppBar
+import com.example.uas_pam.ui.navigation.DestinasiNavigasi
+import com.example.uas_pam.ui.viewmodel.PenyediaViewModel
+import com.example.uas_pam.ui.viewmodel.pekerja.UpdateViewModelPekerja
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UpdateScreenPekerja(
+    onBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    onNavigate:()-> Unit,
+    viewModel: UpdateViewModelPekerja = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiUpdatePekerja.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUP = onBack,
+                subtitle = "Selamat Datang"
+            )
+        }
+    ){padding ->
+        EntryBodyPekerja(
+            modifier = Modifier.padding(padding),
+            insertPKJUiState = viewModel.updatePKJUiState,
+            onPekerjaValueChange = viewModel::updateInsertPkjState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updatePkj()
+                    delay(600)
+                    withContext(Dispatchers.Main){
+                        onNavigate()
+                    }
+                }
+            }
+        )
+    }
+}
