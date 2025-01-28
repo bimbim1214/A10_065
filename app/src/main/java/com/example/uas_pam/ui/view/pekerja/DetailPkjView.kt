28b8c0ded0e5eeea7599bc227cebee263e497188
bimbim1214 <bimoadityapangestu@gmail.com>
2025-01-28 19:49:02 +0700

    DetailScreenPekerja untuk menampilkan informasi detail pekerja, dengan penanganan loading, error, dan data yang sudah berhasil dimuat

--- app/src/main/java/com/example/uas_pam/ui/view/pekerja/DetailPkjView.kt
+++ app/src/main/java/com/example/uas_pam/ui/view/pekerja/DetailPkjView.kt
@@ -31,6 +31,92 @@ import com.example.uas_pam.ui.viewmodel.PenyediaViewModel
 import com.example.uas_pam.ui.viewmodel.pekerja.DetailPKJViewModel
 
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailScreenPekerja(
+    idpekerja: String,
+ //   onEditClick: (String) -> Unit = {},
+    onDeleteClick: (String) -> Unit = {},
+    onBackClick: () -> Unit = {},
+  //  modifier: Modifier = Modifier,
+    viewModel: DetailPKJViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val pekerja = viewModel.DetailpkjUiState.detailPKJEvent
+
+    LaunchedEffect(idpekerja){
+        viewModel.fetchDetailPekerja(idpekerja)
+    }
+
+    val isLoading = viewModel.DetailpkjUiState.isLoading
+    val isError = viewModel.DetailpkjUiState.isError
+    val errorMessage = viewModel.DetailpkjUiState.errorMessage
+
+    Scaffold (
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {Text(DestinasiDetailPekerja.titleRes)},
+                navigationIcon = {
+                    IconButton(onClick = { onBackClick() }) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = "Back"
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors()
+            )
+        },
+//        floatingActionButton = {
+//            FloatingActionButton(
+//                onClick = { onEditClick(pekerja.id_pekerja) }
+//            ) {
+//                Icon(Icons.Default.Edit, contentDescription ="Edit Data")
+//            }
+//        },
+        content = { paddingValues ->
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+            ){
+                if (isLoading){
+                    CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                }
+                else if (isError) {
+                    Text(
+                        text = errorMessage,
+                        style = MaterialTheme.typography.bodyLarge,
+                        modifier = Modifier.align(Alignment.Center)
+                    )
+                }
+                else if (viewModel.DetailpkjUiState.isUiEventNotEmpty) {
+                    Column (
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(16.dp),
+                        verticalArrangement = Arrangement.spacedBy(16.dp),
+                        horizontalAlignment = Alignment.Start
+                    ) {
+                        Card (
+                            modifier = Modifier.fillMaxWidth(),
+                            elevation = CardDefaults.cardElevation(8.dp)
+                        ) {
+                            Column (
+                                modifier = Modifier.padding(16.dp),
+                                verticalArrangement = Arrangement.spacedBy(8.dp)
+                            ) {
+                                DetaipkjlRow(label = "ID Pekerja", value = pekerja.id_pekerja)
+                                DetaipkjlRow(label = "Nama Pekerja", value = pekerja.nama_pekerja)
+                                DetaipkjlRow(label = "Jabatan", value = pekerja.jabatan)
+                                DetaipkjlRow(label = "Kontrak Kerja", value = pekerja.kontak_pekerja)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    )
+}
+
 
 @Composable
 fun DetaipkjlRow(label: String, value: String) {
