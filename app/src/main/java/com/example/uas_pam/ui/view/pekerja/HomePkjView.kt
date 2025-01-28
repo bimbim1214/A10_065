eb19b0c0f16fdf15bedb6e35d23e1782b7d327b7
bimbim1214 <bimoadityapangestu@gmail.com>
2025-01-28 19:39:37 +0700

    fungsi onLoading dan onError untuk menampilkan status loading dan menangani error dengan opsi retry.

--- app/src/main/java/com/example/uas_pam/ui/view/pekerja/HomePkjView.kt
+++ app/src/main/java/com/example/uas_pam/ui/view/pekerja/HomePkjView.kt
@@ -62,6 +62,41 @@ import com.example.uas_pam.ui.viewmodel.pekerja.HomePKJViewModel
 
 
 @Composable
+fun onLoadinng(modifier: Modifier = Modifier){
+    Image(
+        modifier = modifier.size(100.dp),
+        painter = painterResource(R.drawable.loading),
+        contentDescription = stringResource(R.string.loading)
+    )
+}
+
+@Composable
+fun onError(
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    Column (
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.error),
+            contentDescription = "Error Image",
+            modifier = Modifier
+                .size(100.dp) //atur ukuran gambar menajdi 100x100
+        )
+        Text(
+            text = stringResource(R.string.loading_failed),
+            modifier = Modifier.padding(16.dp)
+        )
+        Button(onClick = retryAction) {
+            Text(stringResource(R.string.retry))
+        }
+    }
+}
+
+@Composable
 fun PkjLayout(
     pekerja:List<Pekerja>,
     modifier: Modifier = Modifier,
