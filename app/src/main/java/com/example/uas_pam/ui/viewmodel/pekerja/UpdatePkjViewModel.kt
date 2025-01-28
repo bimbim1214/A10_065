fb194ab705726f672b998935bbded4f04431dc48
bimbim1214 <bimoadityapangestu@gmail.com>
2025-01-28 19:31:32 +0700

    UpdateViewModelPekerja untuk mengelola pembaruan data pekerja.

---
+++ app/src/main/java/com/example/uas_pam/ui/viewmodel/pekerja/UpdatePkjViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.uas_pam.ui.viewmodel.pekerja
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.uas_pam.repository.PekerjaRepository
+import com.example.uas_pam.ui.view.pekerja.DestinasiUpdatePekerja
+import kotlinx.coroutines.launch
+
+class UpdateViewModelPekerja (
+    savedStateHandle: SavedStateHandle,
+    private val pekerjaRepository: PekerjaRepository
+)
