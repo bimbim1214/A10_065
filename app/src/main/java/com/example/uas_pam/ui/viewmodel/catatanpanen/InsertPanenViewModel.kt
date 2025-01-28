577548268e27af48523a36bdcd91d187eca8f408
bimbim1214 <bimoadityapangestu@gmail.com>
2025-01-28 20:58:31 +0700

    fungsi untuk mengonversi InsertPnnUiEvent menjadi CatatanPanen

--- app/src/main/java/com/example/uas_pam/ui/viewmodel/catatanpanen/InsertPanenViewModel.kt
+++ app/src/main/java/com/example/uas_pam/ui/viewmodel/catatanpanen/InsertPanenViewModel.kt
@@ -9,6 +9,49 @@ import com.example.uas_pam.model.CatatanPanen
 import com.example.uas_pam.repository.CatatanPanenRepository
 import kotlinx.coroutines.launch
 
+class InsertPanenViewModel(
+    private val pnn: CatatanPanenRepository,
+    ): ViewModel(){
+
+    var pnnuiState by mutableStateOf(InsertPnnUiState())
+        private set
+
+
+    fun updateInsertPanenState(insertPnnUiEvent: InsertPnnUiEvent){
+        pnnuiState = InsertPnnUiState(insertPnnUiEvent = insertPnnUiEvent)
+    }
+
+    suspend fun insertPnn(){
+        viewModelScope.launch {
+            try {
+                pnn.insertPanen(pnnuiState.insertPnnUiEvent.toPnn())
+            }catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
+
+data class InsertPnnUiState(
+    val insertPnnUiEvent: InsertPnnUiEvent = InsertPnnUiEvent()
+)
+
+data class InsertPnnUiEvent(
+    val id_panen : String = "",
+    val id_tanaman : String = "",
+    val tanggal_panen : String = "",
+    val jumlah_panen: String = "",
+    val keterangan : String = "",
+)
+
+fun InsertPnnUiEvent.toPnn(): CatatanPanen = CatatanPanen(
+    id_panen = id_panen,
+    id_tanaman = id_tanaman,
+    tanggal_panen = tanggal_panen,
+    jumlah_panen = jumlah_panen,
+    keterangan = keterangan
+)
+
 
 fun CatatanPanen.toUiStatePnn(): InsertPnnUiState = InsertPnnUiState(
     insertPnnUiEvent = toInsertPanenUiEvent()
